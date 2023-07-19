d97070b57f0c59aa8a867ba36d720714bfa0b577
HaidarMestrah <10120319@mu.edu.lb>
2023-07-19 13:50:41 +0300

    finalized js functionalities and styles

---
+++ Scripts/script.js
@@ -0,0 +1,74 @@
+let expenseCount = 0;
+$(document).ready(function () {
+  // Add New Expense button click event
+  $(".add-btn").click(function () {
+    $(".form-section").fadeIn(200);
+    $(".form-section").css("display", "flex");
+  });
+
+  // Cancel button click event
+  $(".cancel-action-btn").click(function () {
+    $(".form-section").fadeOut();
+  });
+
+  // Add button click event
+  $(".add-action-btn").click(function () {
+    var expenseName = $("#expense-name").val();
+    var expenseAmount = $("#expense-amount").val();
+
+    // Add new row to expense table
+    var newExpense =
+      "<tr>" +
+      "<td>" +
+      expenseName +
+      "</td>" +
+      "<td>" +
+      expenseAmount +
+      "</td>" +
+      "<td><button class='dlt-btn' id ='dltBtn'>Remove</button></td>" +
+      "</tr>";
+    $("#expense-body").append(newExpense);
+
+    // Clear form fields
+    $("#expense-name").val("");
+    $("#expense-amount").val("");
+
+    expenseCount = expenseCount + 1;
+
+    let total = $("#totalExpenses");
+    total.text(expenseCount);
+    // Hide form
+    $(".form-section").fadeOut();
+    $("#stats-table").css("display", "flex");
+    $("#expense-table").css("display", "flex");
+    $("#empty-list").css("display", "none");
+  });
+
+  // Delete button click event
+  $("#expense-body").on("click", ".dlt-btn", function () {
+    $(this).closest("tr").remove();
+    expenseCount--;
+
+    let total = $("#totalExpenses");
+    total.text(expenseCount);
+
+    if (expenseCount === 0) {
+      $("#stats-table").css("display", "none");
+      $("#expense-table").css("display", "none");
+      $("#empty-list").css("display", "flex");
+    } else {
+      $("#stats-table").css("display", "flex");
+      $("#expense-table").css("display", "flex");
+      $("#empty-list").css("display", "none");
+    }
+  });
+  if (expenseCount === 0) {
+    $("#stats-table").css("display", "none");
+    $("#expense-table").css("display", "none");
+    $("#empty-list").css("display", "flex");
+  }
+});
+
+$(window).on("beforeunload", function () {
+  return "Are you sure you want to leave this page? Your records will be lost.";
+});
